dfa9c872cbdc8e45f2a65ee122a8cb685abb6607
Didi518 <kevin.ouali@gmail.com>
2025-07-23 02:17:37 +0200

    :memo: readme.md

--- backend/config/cors/allowedOrigins.js
+++ backend/config/cors/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ['http://localhost:3000'];
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://photo-flow-vb55.vercel.app',
+];
 
 module.exports = allowedOrigins;
