d9f2542577a48542904154ee050c90cfff21dd96
Didi518 <kevin.ouali@gmail.com>
2025-07-23 03:58:38 +0200

    render

--- backend/config/cors/allowedOrigins.js
+++ backend/config/cors/allowedOrigins.js
@@ -2,6 +2,8 @@ const allowedOrigins = [
   'http://localhost:3000',
   'https://photo-flow-vb55.vercel.app',
   'https://photo-flow.onrender.com',
+  'https://photo-flow-7626xmr8b-didi518s-projects.vercel.app',
+  'https://photo-flow-eight.vercel.app',
 ];
 
 module.exports = allowedOrigins;
